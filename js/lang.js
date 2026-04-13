7872e93f75abf99d5e7c59eda1cc1abc0ba44127
arman <ayka08929@gmail.com>
2026-04-13 16:39:58 +0800

    push

---
+++ js/lang.js
@@ -0,0 +1,15 @@
+﻿function loadLanguage(lang){
+    fetch('./lang/${lang}.json')
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll('[data-i18n]').forEach(el =>{
+                const key = el.getAttribute('data-i18n');
+                if(data[key]){
+                    el.textContent = data[key];
+                }
+            });
+        })
+        .catch(error => console.error('Error Loading Language:', error));
+
+}
+loadLanguage('en');
